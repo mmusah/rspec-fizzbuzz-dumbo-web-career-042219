df0eb5c062f4d462af699ef4882d73b2dce70940
Muhammad Musah <mmusah1@gmail.com>
2019-04-19 02:33:51 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(any)
+  if any % 3 == 0 && any % 5 == 0
+      val="FizzBuzz"
+    elsif any % 3 == 0
+        val="Fizz"
+      elsif any % 5 == 0
+          val="Buzz"
+    else
+      puts "not"
+  end 
