482af940972c80f5b272c318f8e15ac9ac202bf0
Muhammad Musah <mmusah1@gmail.com>
2019-04-19 02:33:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-   for i in 1..100
-	output = i
-	
-	if i % 3 == 0
-		output = "Fizz"
-	end
-
-	if i % 5 == 0
-		output = "Buzz"
-	end
-
-	if (i % 3 == 0 and i % 5 == 0)
-		output = "FizzBuzz"
-	end
-
-	puts output
-end
+def fizzbuzz(any)
+  if any % 3 == 0 && any % 5 == 0
+      val="FizzBuzz"
+    elsif any % 3 == 0
+        val="Fizz"
+      elsif any % 5 == 0
+          val="Buzz"
+    else
+      puts "Not divisible by 3 or 5"
+  end 
