2c83c900eddb9276c3d4b7e81a28bfda04c1197a
Muhammad Musah <mmusah1@gmail.com>
2019-04-19 02:30:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-    def fizzbuzz
-     
-    end
+   for i in 1..100
+	output = i
+	
+	if i % 3 == 0
+		output = "Fizz"
+	end
+
+	if i % 5 == 0
+		output = "Buzz"
+	end
+
+	if (i % 3 == 0 and i % 5 == 0)
+		output = "FizzBuzz"
+	end
+
+	puts output
+end
